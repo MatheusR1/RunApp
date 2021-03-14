96c6db0eb2075f5051b6987a98974b6e1d84b964
MatheusR1 <matheusrocha_456@hotmail.com>
2021-03-14 20:51:35 -0300

    first commit

---
+++ app/src/main/java/com/example/runapp/data/LoginDataSource.kt
@@ -0,0 +1,22 @@
+package com.example.runapp.data
+
+import com.example.runapp.data.model.LoggedInUser
+import java.io.IOException
+
+
+class LoginDataSource {
+
+    fun login(username: String, password: String): Result<LoggedInUser> {
+        try {
+            // TODO: handle loggedInUser authentication
+            val fakeUser = LoggedInUser(java.util.UUID.randomUUID().toString(), "Jane Doe")
+            return Result.Success(fakeUser)
+        } catch (e: Throwable) {
+            return Result.Error(IOException("Error logging in", e))
+        }
+    }
+
+    fun logout() {
+        // TODO: revoke authentication
+    }
+}
---
+++ app/src/main/java/com/example/runapp/data/LoginRepository.kt
@@ -0,0 +1,39 @@
+package com.example.runapp.data
+
+import com.example.runapp.data.model.LoggedInUser
+
+class LoginRepository(val dataSource: LoginDataSource) {
+
+
+    var user: LoggedInUser? = null
+        private set
+
+    val isLoggedIn: Boolean
+        get() = user != null
+
+    init {
+
+        user = null
+    }
+
+    fun logout() {
+        user = null
+        dataSource.logout()
+    }
+
+    fun login(username: String, password: String): Result<LoggedInUser> {
+        // handle login
+        val result = dataSource.login(username, password)
+
+        if (result is Result.Success) {
+            setLoggedInUser(result.data)
+        }
+
+        return result
+    }
+
+    private fun setLoggedInUser(loggedInUser: LoggedInUser) {
+        this.user = loggedInUser
+
+    }
+}
