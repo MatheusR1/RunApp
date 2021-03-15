18a722ffa275f30f22c2ff951f10f106a02ba40f
MatheusR1 <matheusrocha_456@hotmail.com>
2021-03-14 22:21:13 -0300

    add text view SucessLogin

--- app/src/main/java/com/example/runapp/data/Result.kt
+++ app/src/main/java/com/example/runapp/data/Result.kt
@@ -1,9 +1,6 @@
 package com.example.runapp.data
 
-/**
- * A generic class that holds a value with its loading status.
- * @param <T>
- */
+
 sealed class Result<out T : Any> {
 
     data class Success<out T : Any>(val data: T) : Result<T>()
---
+++ app/src/main/java/com/example/runapp/ui/login/SuccessLogin.kt
@@ -0,0 +1,19 @@
+package com.example.runapp.ui.login
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import com.example.runapp.R
+
+class SuccessLogin : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_success_login)
+
+
+        fun SendMessage(view : View){
+
+        }
+    }
+
+}
